ecb4d6f42249110fd6d56424fa9fc0dd48711af2
torstein-vik <torsteinv64@gmail.com>
2018-11-23 17:31:19 +0100

    Added interpreters for assigning bools, strings, and ints

--- scala/src/main/scala/common/interpretation/Interpreter.scala
+++ scala/src/main/scala/common/interpretation/Interpreter.scala
@@ -9,4 +9,15 @@ trait Interpreter {
 }
 
 object Interpreter {
+    def stringInterpreter : Interpreter = new Interpreter {
+        def interpret (path : Path, str : String) = Information.Apply[String](path, str)
+    }
+    
+    def intInterpreter : Interpreter = new Interpreter {
+        def interpret (path : Path, str : String) = Information.Apply[Int](path, str.toInt)
+    }
+    
+    def boolInterpreter : Interpreter = new Interpreter {
+        def interpret (path : Path, str : String) = Information.Apply[Boolean](path, str.toBoolean)
+    }
 }
