1c726a0894b480e3aef81dd38004fb39d0ec7e0a
torstein-vik <torsteinv64@gmail.com>
2018-11-23 20:13:33 +0100

    Added inert NoInformation

--- scala/src/main/scala/common/conceptoids/Information.scala
+++ scala/src/main/scala/common/conceptoids/Information.scala
@@ -6,6 +6,8 @@ import scala.reflect.runtime.universe._
 abstract class Information
 
 object Information {    
+    case object NoInformation extends Information
+    
     case class NewConceptoid(name : String) extends Information
     case class Apply[S](path : Path, value : S)(implicit val typetag : TypeTag[S]) extends Information
     case class OptionAssign(path : Path, possible : Boolean) extends Information
