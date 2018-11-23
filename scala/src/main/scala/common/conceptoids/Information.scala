bf0ead624d51aed9d08e554f4933dea1a2d4ca45
torstein-vik <torsteinv64@gmail.com>
2018-11-23 19:11:34 +0100

    Added OptionAssign-information

--- scala/src/main/scala/common/conceptoids/Information.scala
+++ scala/src/main/scala/common/conceptoids/Information.scala
@@ -8,4 +8,5 @@ abstract class Information
 object Information {    
     case class NewConceptoid(name : String) extends Information
     case class Apply[S](path : Path, value : S)(implicit val typetag : TypeTag[S]) extends Information
+    case class OptionAssign(path : Path, possible : Boolean) extends Information
 }
