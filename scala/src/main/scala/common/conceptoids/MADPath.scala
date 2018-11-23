f2d8e11dcb1042729f72d82ac8d4e24c6bc6fe35
torstein-vik <torsteinv64@gmail.com>
2018-11-23 20:16:42 +0100

    Added MADPath EnterOption

--- scala/src/main/scala/common/conceptoids/MADPath.scala
+++ scala/src/main/scala/common/conceptoids/MADPath.scala
@@ -7,12 +7,14 @@ abstract class MADPath {
         case Destination => ""
         case EnterTree(param, next) => ".\"" + param + "\"" + next.toString
         case EnterList(index, next) => "." + index + next.toString
+        case EnterOption(next) => next.toString
     }
     
     def +(other : MADPath) : MADPath = this match {
         case Destination => other
         case EnterTree(param, next) => EnterTree(param, next + other)
         case EnterList(index, next) => EnterList(index, next + other)
+        case EnterOption(next) => EnterOption(next + other)
     }
 }
 
@@ -22,10 +24,12 @@ object MADPath {
     case object Destination extends MADPath
     case class EnterTree(param : String, next : MADPath) extends MADPath
     case class EnterList(index : Int, next : MADPath) extends MADPath
+    case class EnterOption(next : MADPath) extends MADPath
     
     def navigate(path : MADPath, nav : MADNavigable[Any]) : MADNavigable[Any] = path match {
         case Destination => nav
         case EnterTree(p, next) => navigate(next, nav.attr(p).get)
         case EnterList(i, next) => navigate(next, nav.index(i).get)
+        case EnterOption(next) => navigate(next, nav.getInternalValue)
     }
 }
