b94dc6e7598b6df816dbf33fb3ad94c868b1db29
torstein-vik <torsteinv64@gmail.com>
2018-11-23 15:14:40 +0100

    Added attribute change as a form of Information

--- scala/src/main/scala/common/conceptoids/Information.scala
+++ scala/src/main/scala/common/conceptoids/Information.scala
@@ -1,9 +1,11 @@
 package io.github.nordicmath.mad.conceptoids
 
+import scala.reflect.runtime.universe._
 
 
 abstract class Information
 
 object Information {    
     case class NewConceptoid(name : String) extends Information
+    case class Apply[S : TypeTag](path : Path, value : S) extends Information
 }
