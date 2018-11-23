40601080555b12177342d5ea4fde64dd8017a93e
torstein-vik <torsteinv64@gmail.com>
2018-11-23 15:01:34 +0100

    Added standard IO

--- scala/src/main/scala/common/IO.scala
+++ scala/src/main/scala/common/IO.scala
@@ -5,3 +5,9 @@ trait IO {
     def show(str : String)
 }
 
+object IO {
+    implicit object standardIO extends IO {
+        def show(str : String) = println(str)
+        def read() = scala.io.StdIn.readLine()
+    }
+}
