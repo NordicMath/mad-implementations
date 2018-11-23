9c6dd5e3b71613466e1c2a5da509f53346b052d7
torstein-vik <torsteinv64@gmail.com>
2018-11-23 17:23:30 +0100

    Added generatePath which selects the first path

--- scala/src/main/scala/common/questions/Priority.scala
+++ scala/src/main/scala/common/questions/Priority.scala
@@ -6,6 +6,12 @@ import memory._
 import conceptoids._
 
 object PriorityEngine {
+    def generatePath()(implicit mem : Memory) : Path = {
+        val paths = generatePaths()
+        
+        if(paths.length > 0) paths.apply(0) else throw MADException("No questions!")
+    }
+    
     def generatePaths()(implicit mem : Memory) : Seq[Path] = for {
         (cname, conceptoid) <- mem.getObjects
         sub <- conceptoid.tree.subpaths
