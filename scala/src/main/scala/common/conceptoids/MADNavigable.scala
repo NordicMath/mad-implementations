c5b5dd6fdebdb79a2877956b24fa60fc2c309aed
torstein-vik <torsteinv64@gmail.com>
2018-11-23 15:22:17 +0100

    Finished MADNavigable apply

--- scala/src/main/scala/common/conceptoids/MADNavigable.scala
+++ scala/src/main/scala/common/conceptoids/MADNavigable.scala
@@ -13,6 +13,12 @@ object MADNavigable {
     import MADType._
     
     def apply(x : MADType) : MADNavigable[Any] = x match {
+        case MADString => new MADValue[String]()
+        case MADBool => new MADValue[Boolean]()
+        case MADInt => new MADValue[Int]()
+        case MADTree(name, params @ _*) => new MADValueTree(name, params)
+        case MADList(param) => new MADValueList(param)
+        case MADOption(param) => new MADValueOption(param)
     }
     
     class MADValue[T : TypeTag] () extends MADNavigable[T] {
