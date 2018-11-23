7662cd95c91b18bc4d7831627b195d45b87c8cd3
torstein-vik <torsteinv64@gmail.com>
2018-11-23 15:19:21 +0100

    Added MADValue for general concrete value navigables, and concrete classes for other navigables as well

--- scala/src/main/scala/common/conceptoids/MADNavigable.scala
+++ scala/src/main/scala/common/conceptoids/MADNavigable.scala
@@ -1,5 +1,6 @@
 package io.github.nordicmath.mad.conceptoids
 
+import scala.reflect.runtime.universe._
 import io.github.nordicmath.mad._
 
 trait MADNavigable[+T] {
@@ -12,4 +13,16 @@ object MADNavigable {
     def apply(x : MADType) : MADNavigable[Any] = x match {
     }
     
+    class MADValue[T : TypeTag] () extends MADNavigable[T] {
+    }
+
+    class MADValueTree (name : String, params : Seq[(String, MADType)]) extends MADNavigable[Nothing] {
+    }
+
+    class MADValueList (param : MADType) extends MADNavigable[Nothing] {
+    }
+
+    class MADValueOption (param : MADType) extends MADNavigable[Nothing] {
+    }
+
 }
