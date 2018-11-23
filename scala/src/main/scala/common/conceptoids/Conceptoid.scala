a22ef56c81118b2617a285a6b998b7dfefaf9b87
torstein-vik <torsteinv64@gmail.com>
2018-11-23 15:36:17 +0100

    Added simple MADType for conceptoid

--- scala/src/main/scala/common/conceptoids/Conceptoid.scala
+++ scala/src/main/scala/common/conceptoids/Conceptoid.scala
@@ -9,4 +9,19 @@ object Conceptoid {
     
     def apply() = new Conceptoid()
     
+    lazy val Conceptoid = MADTree("Conceptoid", 
+        "name" -> MADString,
+        "collection structure" -> MADOption(Collection)
+    )
+    
+    lazy val Collection = MADTree("Collection", 
+        "representations" -> MADList(Representation)
+    )
+    
+    lazy val Representation = MADTree("Representation",
+        "description" -> MADString,
+        "schema" -> MADString,
+        "faithful" -> MADBool,
+        "surjective" -> MADBool
+    )
 }
