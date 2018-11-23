dad7f100dd1c4aa880f3fbda024fe108cd3f7bf2
torstein-vik <torsteinv64@gmail.com>
2018-11-23 15:20:44 +0100

    Added madtype so instances can point to their type

--- scala/src/main/scala/common/conceptoids/MADNavigable.scala
+++ scala/src/main/scala/common/conceptoids/MADNavigable.scala
@@ -4,6 +4,8 @@ import scala.reflect.runtime.universe._
 import io.github.nordicmath.mad._
 
 trait MADNavigable[+T] {
+    def madtype : MADType
+    
 }
 
 object MADNavigable {
@@ -14,15 +16,29 @@ object MADNavigable {
     }
     
     class MADValue[T : TypeTag] () extends MADNavigable[T] {
+        def madtype = typeOf[T] match {
+            case t if t =:= typeOf[String] => MADString
+            case t if t =:= typeOf[Boolean] => MADBool
+            case t if t =:= typeOf[Int] => MADInt
+            case t => throw MADException("Type not supported by MAD! " + t)
+        }
+        
     }
 
     class MADValueTree (name : String, params : Seq[(String, MADType)]) extends MADNavigable[Nothing] {
+        
+        def madtype = MADTree(name, params : _*)
+        
     }
 
     class MADValueList (param : MADType) extends MADNavigable[Nothing] {
+        def madtype = MADList(param)
+        
     }
 
     class MADValueOption (param : MADType) extends MADNavigable[Nothing] {
+        def madtype = MADOption(param)
+        
     }
 
 }
