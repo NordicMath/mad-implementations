fb47a20af7f9a66d5771d8769b2cf2a81cc0ee15
torstein <torsteinv64@gmail.com>
2018-11-26 05:18:17 +0100

    Added Path fromJSON

--- scala/src/main/scala/conceptoids/Path.scala
+++ scala/src/main/scala/conceptoids/Path.scala
@@ -1,12 +1,22 @@
 package io.github.nordicmath.mad.conceptoids
 
+import org.json4s._
+
 case class Path(cname : String, mpath : MADPath) {
     override def toString = cname + mpath.toString
     
-    import org.json4s._
-
     def toJSON : JValue = JObject(
         "cname" -> JString(cname),
         "mpath" -> mpath.toJSON
     )
+}
+
+object Path {
+    
+    def fromJSON (x : JValue) = x match {
+        case JObject(List(
+            JField("cname", JString(cname)),
+            JField("mpath", mpath)
+        )) => Path(cname, MADPath.fromJSON(mpath))
+    }
 }
