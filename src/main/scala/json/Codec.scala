967068be19a4484a293bb93eceb4b8e19fc1d178
torstein <torsteinv64@gmail.com>
2018-11-27 03:00:51 +0100

    Added simple structure for codecs

---
+++ src/main/scala/json/Codec.scala
@@ -0,0 +1,16 @@
+package io.github.nordicmath.mad.json
+
+import org.json4s._
+
+
+trait Encoder[-T] {
+    def encode(x : T) : JValue
+}
+
+trait Decoder[+T] {
+    def decode(json : JValue) : T
+}
+
+trait Codec[T] extends Encoder[T] with Decoder[T]
+
+object Codec extends Codecs
---
+++ src/main/scala/json/Codecs.scala
@@ -0,0 +1,7 @@
+package io.github.nordicmath.mad.json
+
+import org.json4s._
+
+trait Codecs {
+    
+}
