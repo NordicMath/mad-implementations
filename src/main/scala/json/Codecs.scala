a063f38048218dfe6c74bcfe17b2584da2a8a052
torstein-vik <torsteinv64@gmail.com>
2018-11-29 15:11:19 +0100

    Added SCodec case class

--- src/main/scala/json/Codecs.scala
+++ src/main/scala/json/Codecs.scala
@@ -2,6 +2,15 @@ package io.github.nordicmath.mad.json
 
 import org.json4s._
 
+import util.Try
+
 trait Codecs {
+    case class SCodec[S <: JValue, T](encoder : T => S, decoder : S => Option[T]) extends Codec[T] {
+        def embed (j : JValue) : Option[S] = Try(j.asInstanceOf[S]).toOption
+        
+        def encode(t : T) = encoder(t)
+        def decode(j : JValue) = for {s <- embed(j); t <- decoder(s)} yield t
+    }
+        
     
 }
