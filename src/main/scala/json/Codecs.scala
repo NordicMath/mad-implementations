b5d406c69bac8de49b1c3e0e55894ad09c81a7d9
torstein <torsteinv64@gmail.com>
2018-12-02 15:46:28 +0100

    Added list and seq codecs

--- src/main/scala/json/Codecs.scala
+++ src/main/scala/json/Codecs.scala
@@ -5,6 +5,7 @@ import org.json4s._
 import util.Try
 
 trait Codecs {
+    // Codec helpers
     case class SCodec[S <: JValue, T](encoder : T => S, decoder : S => Option[T]) extends Codec[T] {
         def embed (j : JValue) : Option[S] = Try(j.asInstanceOf[S]).toOption
         
@@ -16,11 +17,25 @@ trait Codecs {
         def encode(t : T) = JSON[S](to(t))
         def decode(j : JValue) = JSON[S].unapply(j).map(from)
     }
-        
+    
+    // Primitive codecs
     implicit object StringCodec extends SCodec[JString, String](JString.apply, JString.unapply)
     implicit object BooleanCodec extends SCodec[JBool, Boolean](JBool.apply, JBool.unapply)
     implicit object BigIntCodec extends SCodec[JInt, BigInt](JInt.apply, JInt.unapply)
     
     implicit object IntCodec extends TCodec[Int, BigInt](BigInt(_), _.toInt)
+    
+    // Compound codecs
+    implicit def listCodec[T : Codec] = new ListCodec[T]
+    class ListCodec[T : Codec] extends SCodec[JArray, List[T]] (
+        lst => JArray(lst.map(JSON[T])),
+        jar => jar.arr.map(JSON[T].unapply) match {
+            case x if x.contains(None) => None
+            case x => Some(x.flatten)
+        }
+    )
+    
+    implicit def seqCodec[T : Codec] = new SeqCodec[T]
+    class SeqCodec[T : Codec] extends TCodec[Seq[T], List[T]] (_.toList, _.toSeq)
     
 }
