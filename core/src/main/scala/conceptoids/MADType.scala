9832d6386a8c21773a48c0da429d9f3417129d22
torstein <torsteinv64@gmail.com>
2018-12-04 17:32:16 +0100

    Added flatten

--- core/src/main/scala/conceptoids/MADType.scala
+++ core/src/main/scala/conceptoids/MADType.scala
@@ -3,6 +3,10 @@ package io.github.nordicmath.mad.conceptoids
 sealed abstract class MADType(val name : String)
 
 object MADType {
+    import scala.language.implicitConversions
+    implicit def enrich(tp : MADType) : RichMADType = RichMADType(tp)
+    implicit def flatten(rich : RichMADType) = rich.inner
+    
     case object MADString extends MADType("String")
     case object MADBool extends MADType("Boolean")
     case object MADInt extends MADType("Int")
@@ -10,6 +14,4 @@ object MADType {
     case class MADList(param : MADType) extends MADType("List(" + param.name + ")")
     case class MADOption(param : MADType) extends MADType("Option(" + param.name + ")")
     
-    import scala.language.implicitConversions
-    implicit def enrich(tp : MADType) : RichMADType = RichMADType(tp)
 }
