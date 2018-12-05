cf525b559f3a381defa0362097d22e941f758f6c
torstein <torsteinv64@gmail.com>
2018-12-05 15:03:14 +0100

    Implemented Inner MAD-navigation

--- core/src/main/scala/conceptoids/MADPath.scala
+++ core/src/main/scala/conceptoids/MADPath.scala
@@ -23,6 +23,9 @@ object MADPath {
     case class Destination(override val madtype : RichMADType) extends MADPath(madtype, None){
         def validate = true
         
+        type Nav = MADNavigable
+        def inner_navigate (nav : MADNavigable) = nav
+        
     }
     
     case class EnterTree(param : String, next : MADPath, override val madtype : RichMADType) extends MADPath(madtype, Some(next)){
@@ -31,15 +34,24 @@ object MADPath {
             case _ => false
         }
         
+        type Nav = MADValueTree
+        def inner_navigate (nav : MADValueTree) = next.navigate(nav.attr(param).get)
+        
     }
     
     case class EnterList(index : Int, next : MADPath, override val madtype : RichMADType) extends MADPath(madtype, Some(next)){
         def validate = madtype.inner.isInstanceOf[MADList]
         
+        type Nav = MADValueList
+        def inner_navigate (nav : MADValueList) = next.navigate(nav.index(index).get)
+        
     }
     
     case class EnterOption(next : MADPath, override val madtype : RichMADType) extends MADPath(madtype, Some(next)){
         def validate = madtype.inner.isInstanceOf[MADOption]
+        
+        type Nav = MADValueOption
+        def inner_navigate (nav : MADValueOption) = next.navigate(nav.getInternalValue)
         
     }
 }
