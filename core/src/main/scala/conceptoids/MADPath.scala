cae66193b47d80f1296afc041544086f09d2dcb1
torstein <torsteinv64@gmail.com>
2018-12-05 15:01:28 +0100

    Added basic methods to MADPath

--- core/src/main/scala/conceptoids/MADPath.scala
+++ core/src/main/scala/conceptoids/MADPath.scala
@@ -4,9 +4,13 @@ import io.github.nordicmath.mad._
 import MADNavigable._
 import MADType._
 
-sealed abstract class MADPath {
+sealed abstract class MADPath(val madtype : RichMADType, next : Option[MADPath]) {
+    type Nav <: MADNavigable 
     
+    final def navigate (nav : MADNavigable) : MADNavigable = nav match {
     }
+    protected def inner_navigate (nav : Nav) : MADNavigable
+    
 }
 
 object MADPath {
