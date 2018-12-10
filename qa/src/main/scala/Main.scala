006a5948a2b67f0ec2fe16f7fac47bd0a6203549
torstein <torsteinv64@gmail.com>
2018-12-10 12:29:10 +0100

    Made Main refer to conceptoid as memory type

--- qa/src/main/scala/Main.scala
+++ qa/src/main/scala/Main.scala
@@ -2,10 +2,11 @@ package io.github.nordicmath.mad.qa
 
 import io.github.nordicmath.mad._
 import memory._
+import conceptoids._
 
 object Main extends App {
     
-    implicit val memory = Memory()
+    implicit val memory = Memory(Conceptoid.Conceptoids)
     
     QA().start()
     
