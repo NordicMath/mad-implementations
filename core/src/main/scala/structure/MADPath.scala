0ff34ee62ad15ed7db99b831f35045408914d7bd
torstein-vik <torsteinv64@gmail.com>
2018-12-14 17:41:59 +0100

    Added MADPathInstruction ADT

--- core/src/main/scala/structure/MADPath.scala
+++ core/src/main/scala/structure/MADPath.scala
@@ -3,3 +3,9 @@ package io.github.nordicmath.mad.structure
 import io.github.nordicmath.mad._
 import MADNavigable._
 import MADType._
+
+sealed abstract class MADPathInstruction (override val toString : String) 
+case class EnterTree(param : String) extends MADPathInstruction(f""""$param"""")
+case class EnterList(index : Int) extends MADPathInstruction(index.toString)
+case object EnterOption extends MADPathInstruction("EnterOption")
+
