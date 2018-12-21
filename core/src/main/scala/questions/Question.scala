5aa3cd21e293f75c84e034c776101f4d83fcd1ec
torstein-vik <torsteinv64@gmail.com>
2018-12-21 16:07:23 +0100

    Small question wording change

--- core/src/main/scala/questions/Question.scala
+++ core/src/main/scala/questions/Question.scala
@@ -15,11 +15,11 @@ object QuestionEngine {
     
     private def fromMADType(p : MADPath, madtype : RichMADType) : Seq[Question] = madtype.inner match {
         case MADString => Seq(Question(f"What is $p?", stringInterpreter(p)))
-        case MADBool => Seq(Question(f"Is $p true/yes or false/no?", boolInterpreter(p)))
+        case MADBool => Seq(Question(f"Is $p?", boolInterpreter(p)))
         case MADInt => Seq(Question(f"What number is $p?", intInterpreter(p)))
         case MADOption(_) => Seq(Question(f"Is something like $p possible?", optionInterpreter(p)))
         case MADList(_) => Seq(Question(f"Are there more elements in $p?", listInterpreter(p)))
-        case MADMap(_) => Seq(Question(f"Please name another element of $p:", mapInterpreter(p)))
+        case MADMap(_) => Seq(Question(f"Please name another element of $p", mapInterpreter(p)))
         
         case MADTree(_, _*) => Seq()
     }
