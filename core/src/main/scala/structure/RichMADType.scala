668a58a6d6c59aaf20980542b3182b3f7c289fe7
torstein <torsteinv64@gmail.com>
2019-02-22 06:08:47 +0100

    Added RichMADType toString and equals

--- core/src/main/scala/structure/RichMADType.scala
+++ core/src/main/scala/structure/RichMADType.scala
@@ -1,8 +1,13 @@
 package io.github.nordicmath.mad.structure
 
 class RichMADType(madtype : => MADType, val info : MADTypeInformation) {
-    def inner = madtype
+    lazy val inner = madtype
     def priority(p : Int) = RichMADType(inner, info.copy(priority = p))
+    override def toString = inner.toString
+    override def equals(other : Any) = other match {
+        case x : RichMADType => x.inner == inner && x.info == info
+        case _ => false
+    }
 }
 
 object RichMADType {
