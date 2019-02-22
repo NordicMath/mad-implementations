4fe26ccc3dc61b48ff9d49bc1b5ac4a68434b277
torstein <torsteinv64@gmail.com>
2019-02-22 07:33:23 +0100

    Added some work on PredicateValue

--- core/src/main/scala/structure/predicate/Predicate.scala
+++ core/src/main/scala/structure/predicate/Predicate.scala
@@ -11,6 +11,7 @@ object Predicate {
     case class SubPredicate (path : MADPath, pred : Predicate) extends Predicate
     case class AndPredicate (pred1 : Predicate, pred2 : Predicate) extends Predicate
     object IsDefined extends Predicate
+    case class IsValue[T] (value : PredicateValue[T]) extends Predicate
     
     
     def validate (madtype : MADType, pred : Predicate) : Boolean = pred match {
@@ -27,4 +28,19 @@ object Predicate {
         case AndPredicate(pred1, pred2) => eval(nav, pred1) && eval(nav, pred2)
         case IsDefined => nav.asInstanceOf[MADValueOption].exists
     }
+}
+
+abstract sealed class PredicateValue[T](val t : T, val valueType : MADType)
+object PredicateValue {
+    import language.implicitConversions
+    
+    import MADNavigable._
+    implicit def madFixedValue[T : MADValuePrimitive](value : T) = MADFixedValue[T](value)
+    case class MADFixedValue[T](value : T)(implicit t : MADValuePrimitive[T]) extends PredicateValue[T](value, t.madtype)
+    
+    implicit def madRefPointer(p : Pointer) : PredicateValue[Pointer] = MADRefPointer(p)
+    case class MADRefPointer(p : Pointer) extends PredicateValue[Pointer](p, MADRef(???))
+    
+    implicit def sequence[T](seq : Seq[T])(implicit conv : T => PredicateValue[T]) = MADSequenceValue(seq.map(conv))
+    case class MADSequenceValue[T](seq : Seq[PredicateValue[T]]) extends PredicateValue[Seq[PredicateValue[T]]](seq, MADList(???))
 }
