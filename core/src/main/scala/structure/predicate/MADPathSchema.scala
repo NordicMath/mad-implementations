d72249ded081dd408f2e0cb0b4665a080687761c
torstein <torsteinv64@gmail.com>
2019-02-22 04:34:28 +0100

    Added MADPathSchema check

--- core/src/main/scala/structure/predicate/MADPathSchema.scala
+++ core/src/main/scala/structure/predicate/MADPathSchema.scala
@@ -11,6 +11,11 @@ case class MADPathSchema(prepath : MADPath) {
         case _ => throw MADException.MADTypeNotIterable(prepath.madtype)
     }
     
-    
+    def check(subpath : MADPath) : Boolean = {
+        subpath.on == prepath.on && 
+        subpath.madtype == next && 
+        subpath.instructions.startsWith(prepath.instructions) && 
+        subpath.instructions.length == prepath.instructions.length + 1
+    }
 }
 
