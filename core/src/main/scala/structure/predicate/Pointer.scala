7a7caf1f175f646334ba07f33b22c24537490d08
torstein-vik <torsteinv64@gmail.com>
2019-03-17 23:15:15 +0100

    Added resolvePath to Pointer

--- core/src/main/scala/structure/predicate/Pointer.scala
+++ core/src/main/scala/structure/predicate/Pointer.scala
@@ -3,10 +3,18 @@ package io.github.nordicmath.mad.structure.predicate
 import io.github.nordicmath.mad._
 import structure._
 
-abstract sealed class Pointer 
+abstract sealed class Pointer {
+    def resolvePath (nav : MADPath) : MADPath
+}
+
 object Pointer {
-    case class Self(schema : MADPathSchema) extends Pointer
-    case class Concrete(mad : MADPath) extends Pointer
+    case class Self(schema : MADPathSchema) extends Pointer {
+        def resolvePath (path : MADPath) = schema.fit(path)
+    }
+    
+    case class Concrete(mad : MADPath) extends Pointer {
+        def resolvePath (path : MADPath) = mad
+    }
     
     import language.implicitConversions
     implicit def concrete(path : MADPath) : Pointer = Concrete(path)
