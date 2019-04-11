dbee71b0335dc20a9571764359fb1f07008ec8c2
torstein-vik <torsteinv64@gmail.com>
2019-04-11 16:39:41 +0200

    Added MADController

---
+++ web/app/controllers/MADController.scala
@@ -0,0 +1,19 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import play.twirl.api._
+
+import io.github.nordicmath.mad._
+import memory._
+import structure._
+import spec._
+import interpretation._
+import web.api._
+
+
+@Singleton
+class MADController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+}
