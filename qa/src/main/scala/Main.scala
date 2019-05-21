9078898cc11d4c04764787c85841816b2eae1f5b
Torstein Vik <torsteinv64@gmail.com>
2019-05-22 00:11:19 +0200

    Updated QA Main to use Spec

--- qa/src/main/scala/Main.scala
+++ qa/src/main/scala/Main.scala
@@ -3,12 +3,11 @@ package io.github.nordicmath.mad.qa
 import io.github.nordicmath.mad._
 import memory._
 import spec._
-import structure._
 
 object Main extends App {
     
-    implicit val madtype : RichMADType = Conceptoid.Conceptoids
-    implicit val memory = Memory(madtype)
+    implicit val spec : Spec = Conceptoid
+    implicit val memory = Memory(spec)
     
     QA().start()
     
