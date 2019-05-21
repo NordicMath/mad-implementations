65b41e41a6345ac4ba5f11748e4cd2251984c8d8
Torstein Vik <torsteinv64@gmail.com>
2019-05-22 00:25:56 +0200

    Implemented resolve for all predicate values

--- core/src/main/scala/structure/predicate/Predicate.scala
+++ core/src/main/scala/structure/predicate/Predicate.scala
@@ -44,11 +44,19 @@ object PredicateValue {
     implicit def madFixedValue[T : MADValuePrimitive](value : T) = MADFixedValue[T](value)
     case class MADFixedValue[T](value : T)(implicit t : MADValuePrimitive[T]) extends PredicateValue {
         def validate (madtype : MADType) = t.madtype == madtype
+        
+        def resolve (path : MADPath, nav : MADNavigable, resolvepath : MADPath) : Boolean = {
+            nav.asInstanceOf[MADValue[T]].getOpt.fold(false)(_ == value)
+        }
     }
     
     implicit def madRefPointer(p : Pointer) = MADRefPointer(p)
     case class MADRefPointer(p : Pointer) extends PredicateValue {
         def validate (madtype : MADType) = madtype.isInstanceOf[MADRef]
+        
+        def resolve (path : MADPath, nav : MADNavigable, resolvepath : MADPath) : Boolean = {
+            nav.asInstanceOf[MADValueRef].getOpt.fold(false)(_ == p.resolvePath(resolvepath))
+        }
     }
     
     implicit def sequence[T](seq : Seq[T])(implicit conv : T => PredicateValue) = MADSequenceValue(seq.map(conv))
@@ -58,5 +66,11 @@ object PredicateValue {
             case _ => false
         }
         
+        def resolve (path : MADPath, nav : MADNavigable, resolvepath : MADPath) : Boolean = {
+            val navlist = nav.asInstanceOf[MADValueList].navlist
+            navlist.length == seq.length && (navlist zip seq).forall {
+                case (subnav, predval) => predval.resolve(path, subnav, resolvepath)
+            }
+        }
     }
 }
